a836279c0d5eff37832ff055ce019b25a4aac83c
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2017-12-24 18:57:57 +0800

    Change license to MIT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject pimote-api "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.3"]
                  [bidi "2.1.2"]]
